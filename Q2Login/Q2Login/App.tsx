c751e820322c0be202724dc64d743f5bdc9868d0
Iamnotbao <ohshit30102015@gmail.com>
2023-12-02 14:48:11 +0700

    Lab2_Update

---
+++ Q2Login/Q2Login/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { View,Text } from 'react-native';
+import LoginScreen from './Login/Login';
+
+ export default App =()=>{
+    return(
+      //  <View><Text>123</Text></View>
+        <LoginScreen/>
+    );
+ };
