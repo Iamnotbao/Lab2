b25b374c4167ee92a5406d359cde84bdc6e53fab
Iamnotbao <ohshit30102015@gmail.com>
2023-12-02 14:45:23 +0700

    Lab2

---
+++ Q3Calculator/Q3Calculator/CalculatorSimulator/Style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+export default styles =StyleSheet.create({
+    container: {
+        flex :1,
+        justifyContent:'center',
+        alignItem:'center',
+        padding:16,
+    },
+    input :{
+        width:200,
+        height:40,
+        borderColor:'black',
+        borderWidth:1,
+        marginBottom:10,
+        padding:10,
+    },
+    result:{
+        marginTop:20,
+        fontSize:16,
+    },
+
+});
